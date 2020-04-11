ee3d8ab3aff312900d65fb6508fdee77eb418991
thecosmos <deepakrajko14@gmail.com>
2020-04-11 20:49:38 +0530

    Add decorators to pre check request for JSON data and required keys in JSON body

--- app/auth.py
+++ app/auth.py
@@ -11,6 +11,7 @@ bp = Blueprint("auth", __name__,)
 @bp.route("/auth/", methods=["GET", "POST"])
 def authorize():
     expires_at = datetime.datetime.now() + datetime.timedelta(seconds=current_app.config["JWT_TOKEN_EXPIRES_IN"])
+    # TODO: Make sure expiration works.
     token = jwt.encode({
         "exp": expires_at,
     }, key=current_app.config["JWT_SECRET_KEY"])
@@ -37,3 +38,21 @@ def requires_authorization(view):
         return view(*args, **kwargs)
 
     return wrapper
+
+
+def check_request(*required_keys):
+
+    def decorator(view):
+
+        @functools.wraps(view)
+        def wrapper(*args, **kwargs):
+            if not request.is_json:
+                return jsonify(message="Invalid JSON body."), 400
+            missing_keys = set(required_keys) - set(request.json.keys())
+            if missing_keys:
+                return jsonify(message="Missing required keys for book entry.", missing_keys=list(missing_keys)), 400
+            return view(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
